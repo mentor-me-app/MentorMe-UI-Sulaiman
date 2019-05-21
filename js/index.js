d4499d0993e287b4403484c6a4c20fb69a145e2f
abidex4yemi <abidex4yemi@gmail.com>
2019-05-21 20:13:52 +0100

    Complete Home page (mvp)

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const menuBar = document.querySelector('.icon-bar');
+const nav = document.querySelector('.nav');
+const closeBtn = document.querySelector('.close-menu');
+
+menuBar.addEventListener('click', evt => {
+	evt.stopPropagation();
+	nav.classList.toggle('hidden');
+});
+
+window.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	if (evt.target !== nav || evt.target !== menuBar || evt.target === closeBtn) {
+		nav.classList.toggle('hidden');
+	}
+});
